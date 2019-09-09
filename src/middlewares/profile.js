cb67088173c24be08179da9696bcfc6b0b79206a
naimishverma17 <naimishverma50@gmail.com>
2019-09-09 20:54:41 +0530

    middleware logic changed

--- src/middlewares/profile.js
+++ src/middlewares/profile.js
@@ -1,10 +1,18 @@
 const Profile = require('../models/profile');
 
 const verifyUsername = (req, res, next) => {
-    Profile.findOne({ username: req.body.username })
+    Profile.findOne({ user: req.user.id })
         .then(profile => {
-            if (profile) {
-                res.status(400).json({ message: "Username already exists!" });
+            if (profile && (profile.username != req.body.username)) {
+                Profile.findOne({ username: req.body.username })
+                    .then(profile => {
+                        if (profile) {
+                            res.status(400).json({ message: "Username already exists!" })
+                        } else {
+                            next();
+                        }
+                    })
+                    .catch(err => next(err))
             } else {
                 next();
             }
