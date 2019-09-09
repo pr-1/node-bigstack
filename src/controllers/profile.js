5c9221d3d0a34d178462eacc0a65c1f6903ea2b6
naimishverma17 <naimishverma50@gmail.com>
2019-09-09 20:11:25 +0530

    debug

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,27 +1,27 @@
 const Profile = require('../models/profile');
 
 const updateProfile = (req, res, next) => {
-    const profile = {};
+    const userProfile = {};
     console.log(req.body);
     console.log(req.user.id);
-    profile.user = req.user.id;
+    userProfile.user = req.user.id;
 
     // General information
     if (req.body.username)
-        profile.username = req.body.username;
+        userProfile.username = req.body.username;
     if (req.body.website)
-        profile.website = req.body.website;
+        userProfile.website = req.body.website;
     if (req.body.languages && req.body.languages.length > 0)
-        profile.languages = req.body.languages;
+        userProfile.languages = req.body.languages;
 
     // Social information
-    profile.social = {};
+    userProfile.social = {};
     if (req.body.facebook)
-        profile.social.facebook = req.body.facebook;
+        userProfile.social.facebook = req.body.facebook;
     if (req.body.youtube)
-        profile.social.youtube = req.body.youtube;
+        userProfile.social.youtube = req.body.youtube;
     if (req.body.instagram)
-        profile.social.instagram = req.body.instagram;
+        userProfile.social.instagram = req.body.instagram;
 
     Profile.findOne({ user: req.user.id })
         .then(profile => {
@@ -30,14 +30,14 @@ const updateProfile = (req, res, next) => {
                 console.log("1", profile);
                 Profile.findOneAndUpdate(
                     { user: req.user.id },
-                    { $set: profile },
+                    { $set: userProfile },
                     { new: true }
                 )
                     .then(p => res.json(p))
                     .catch(err => next(err));
             } else {
                 // Save the new user profile
-                new Profile(profile)
+                new Profile(userProfile)
                     .save()
                     .then(p => res.json({ status: "Profile updated sucessfully", profile: p }))
                     .catch(err => next(err));
